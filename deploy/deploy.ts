f3819b67b3aa82b9f7815d9315436a47ee434fdb
p-zotos <pav.zotos@gmail.com>
2025-11-04 02:13:03 +0200

    Add deploy script for FHEConfidentialVote contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const totalProposals = 3;
+
+  const deployedFHEConfidentialVote = await deploy("FHEConfidentialVote", {
     from: deployer,
     log: true,
+    args: [totalProposals],
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`FHEConfidentialVote contract: `, deployedFHEConfidentialVote.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_fhrConfidentialVote"; // id required to prevent reexecution
+func.tags = ["FHEConfidentialVote"];
